d9f8ff10e5d888e3112753ce2c9cb76b332f2814
Nero Siva <nero.siva@bbc.co.uk>
2018-11-23 13:41:36 +0000

    Add initial fetch for data

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,14 +2,35 @@ import React, {
     Component
 } from 'react';
 
+
+const localStyles = {
+    background: "#C8102E",
+    display: "flex",
+    height: "100%"
+}
 class Homepage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    componentWillMount = () => {
+        fetch('https://api.football-data.org/v1/competitions')
+            .then((response) => response.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
+
+
     render() {
         return (
-            <div>
-                HELLO WORLD!!!
+            <div style={localStyles}>
+                hello world
             </div>
         );
     }
 }
+
 
 export default Homepage;
