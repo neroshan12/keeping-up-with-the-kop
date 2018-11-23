cf90288bafcf184f83a27ae076a71140790dc745
Nero Siva <nero.siva@bbc.co.uk>
2018-11-23 14:05:12 +0000

    Add access token and return Liverpool squad players

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,4 @@
-import React, {
-    Component
-} from 'react';
+import React, { Component } from 'react';
 
 
 const localStyles = {
@@ -11,14 +9,17 @@ const localStyles = {
 class Homepage extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            squad: []
+        }
     }
 
     componentWillMount = () => {
-        fetch('https://api.football-data.org/v1/competitions')
+        fetch("https://api.football-data.org/v2/teams/64", { headers: { "X-Auth-Token": process.env.REACT_APP_FOOTY_API_KEY } })
             .then((response) => response.json())
             .then(data => {
-                console.log(data)
+                this.setState({ squad: data.squad })
+                console.log(this.state.squad)
             })
     }
 
