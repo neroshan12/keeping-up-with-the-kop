333d51110ea93f1cb66990938e88139df3681cfa
Nero Siva <nero.siva@bbc.co.uk>
2018-12-07 16:20:54 +0000

    Add date to fixtures

--- src/components/Fixtures.js
+++ src/components/Fixtures.js
@@ -19,21 +19,30 @@ class Fixtures extends Component {
             })
     }
 
+    reverseString = (string) => {
+        return string.substring(0, 10).split("-").reverse().join("-")
+    }
+
 
     render() {
+
         return (
             <div className="Fixtures_container">
                 <div className="Fixtures_title">Fixtures</div>
                 {this.state.fixtures.map((match, key) => {
                     if (match.status === 'FINISHED') {
                         return <div key={key} className="Fixtures_holder_done">
-                            <div className="Fixtures_player">{match.homeTeam.name} {match.score.fullTime.homeTeam}</div>
-                            <div className="Fixtures_player">- {match.score.fullTime.awayTeam} {match.awayTeam.name}</div>
+                            <div className="Fixtures_game">{match.homeTeam.name} </div>
+                            <div className="Fixtures_score">{match.score.fullTime.homeTeam} - {match.score.fullTime.awayTeam}</div>
+                            <div className="Fixtures_game"> {match.awayTeam.name}</div>
+                            <div className="Fixtures_date"> {this.reverseString(match.utcDate)}</div>
                         </div>
                     } else {
                         return <div key={key} className="Fixtures_holder">
-                            <div className="Fixtures_player">{match.homeTeam.name}</div>
-                            <div className="Fixtures_player">vs {match.awayTeam.name}</div>
+                            <div className="Fixtures_game">{match.homeTeam.name} </div>
+                            <div className="Fixtures_score">{match.score.fullTime.homeTeam} - {match.score.fullTime.awayTeam}</div>
+                            <div className="Fixtures_game"> {match.awayTeam.name}</div>
+                            <div className="Fixtures_date"> {this.reverseString(match.utcDate.substring(0, 10))}</div>
                         </div>
                     }
                 })
